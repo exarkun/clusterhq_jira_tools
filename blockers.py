063bf0e57a9ecdc1f5b1f1bea92e3c0c465a4d25
Jonathan Lange <jml@mumak.net>
2015-10-09 16:42:28 +0100

    Clarity and speedups
    * Move link discovery logic to separate function
    * Only traverse the links we want to find
    * Get rid of worse-than-useless `seen` so we only fetch unseen issues

--- blockers.py
+++ blockers.py
@@ -12,75 +12,58 @@ import issuelib
 NOT_FOUND = object()
 
 
-def build_issue_graph(client, issue):
+def build_issue_graph(client, issue, follow_links=None):
     """
     Build a graph of linked issues, starting at 'issue'.
     """
-    seen = {}
     queue = [issue]
     g = networkx.DiGraph()
     while queue:
         head, queue = queue[0], queue[1:]
-        links = getattr(head.fields, 'issuelinks', ())
 
         g.add_node(head.key, issue=head.raw)
 
-        for link in links:
+        for dest_key, outward, link_type in iter_links(head):
 
-            if getattr(link, 'inwardIssue', NOT_FOUND) is NOT_FOUND:
-                dest_key = link.outwardIssue.key
-                outward = True
-            else:
-                dest_key = link.inwardIssue.key
-                outward = False
+            if follow_links and link_type not in follow_links:
+                continue
 
-            if dest_key not in seen:
-                # XXX: IO
+            if dest_key not in g:
                 dest = client.issue(dest_key)
                 g.add_node(dest_key, issue=dest.raw)
                 queue.append(dest)
 
-            link_type = link.type.name
             if outward:
                 g.add_edge(head.key, dest_key, link_type=link_type)
             else:
                 g.add_edge(dest_key, head.key, link_type=link_type)
 
-        seen[head.key] = head
-
     return g
 
 
-def filter_edges(predicate, graph):
+def iter_links(issue):
     """
-    Create a graph based on 'graph', but only with edges that match
-    'predicate'.
+    Iterate over issue links.
 
-    Any nodes that are no longer connected will also be removed.
+    For each link, yield (key, outward, link_type), where 'key' is the ticket
+    key (e.g. FLOC-2008), 'outward' is the direction of the link, and
+    'link_type' is the name of the type of link (e.g. 'Blocks').
     """
-    nonmatching_edges = (
-        (src, dst) for (src, dst) in graph.edges()
-        if not predicate(graph[src][dst]))
-    new_graph = graph.copy()
-    new_graph.remove_edges_from(nonmatching_edges)
-    new_graph.remove_nodes_from(networkx.isolates(new_graph))
-    return new_graph
-
-
-def descendants_dag(graph, root):
-    # XXX: There must be a standard name for this sort of thing.
-    assert_dag(graph)
-    descs = networkx.descendants(graph, root)
-    non_descs = set(graph.nodes()) - descs
-    non_descs.remove(root)
-    new_graph = graph.copy()
-    new_graph.remove_nodes_from(non_descs)
-    assert_dag(graph)
-    return new_graph
+    links = getattr(issue.fields, 'issuelinks', ())
+    for link in links:
+        if getattr(link, 'inwardIssue', NOT_FOUND) is NOT_FOUND:
+            dest_key = link.outwardIssue.key
+            outward = True
+        else:
+            dest_key = link.inwardIssue.key
+            outward = False
+        yield dest_key, outward, link.type.name
 
 
 def ancestors_dag(graph, root):
-    # XXX: This is a clone of descendants_dag. :(
+    """
+    Return a directed, acyclic graph of all the ancestors of 'root'.
+    """
     assert_dag(graph)
     ancestors = networkx.ancestors(graph, root)
     non_ancestors = set(graph.nodes()) - ancestors
@@ -160,23 +143,15 @@ def print_tree(node, format_node, get_children):
     print format_tree(node, format_node, get_children)
 
 
-def load_graph_from_jira(issue_key):
+def main():
+    issue_key = 'FLOC-2008'
     client = issuelib.client()
     # XXX: IO
     base_issue = client.issue(issue_key)
-    return build_issue_graph(client, base_issue)
-
-
-def load_graph_from_file(path):
-    return networkx.read_gpickle(path)
+    g = build_issue_graph(client, base_issue, follow_links=['Blocks'])
+    g = ancestors_dag(g, issue_key)
+    print_tree(issue_key, partial(format_issue, g), g.predecessors)
 
 
 if __name__ == '__main__':
-    issue_key = 'FLOC-2008'
-    g = load_graph_from_jira(issue_key)
-    # XXX: Useful to be able to save at this point for later processing:
-    # networkx.write_gpickle(g, 'FLOC-2008-closure.pickle')
-    g = filter_edges(lambda x: x.get('link_type') == 'Blocks', g)
-    g = ancestors_dag(g, issue_key)
-    # XXX: print this as a tree, highlighting nodes that appear more than once
-    print_tree(issue_key, partial(format_issue, g), g.predecessors)
+    main()
